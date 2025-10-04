40638bdc18c2951ec85178c64429915bfc508191
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-10-04 20:46:31 +0530

    removed db repo

--- apps/dashboard-b2c/src/app/sample/page.tsx
+++ apps/dashboard-b2c/src/app/sample/page.tsx
@@ -1,9 +1,6 @@
-import { prisma } from "@repo/db"
 
 export default async function Page() {
-	const users = await prisma.user.findMany()
-	console.log("USERS:", users)
 	return (
-		<div>{users.length > 0 ? users[0].email : "No USERS YET"}</div>
+		<div></div>
 	)
 }
