9c9781366aefc240327cc1e8bc39cc60b1627ffe
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-10-04 20:59:02 +0530

    push check

--- apps/client-control-panel/src/app/sample/page.tsx
+++ apps/client-control-panel/src/app/sample/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from "@repo/db"
 
 export default async function Page() {
 	const users = await prisma.user.findMany()
-	console.log("USERS:", users)
+	console.log("USERS:kkk", users)
 	return (
 		<div>{users.length > 0 ? users[0].email : "No USERS YET"}</div>
 	)
