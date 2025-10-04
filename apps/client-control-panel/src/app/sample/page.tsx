bd7f3684eee517f7158c6794345edb350ec68f13
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-10-04 19:46:29 +0530

    added prisma

---
+++ apps/client-control-panel/src/app/sample/page.tsx
@@ -0,0 +1,9 @@
+import { prisma } from "@repo/db"
+
+export default async function Page() {
+	const users = await prisma.user.findMany()
+	console.log("USERS:", users)
+	return (
+		<div>{users.length > 0 ? users[0].email : "No USERS YET"}</div>
+	)
+}
