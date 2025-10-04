611d0d0dfc4dc6bf4562ac8d12a3b3291f528e3d
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-10-04 20:17:35 +0530

    debugging sample page

--- apps/client-control-panel/src/app/sample/page.tsx
+++ apps/client-control-panel/src/app/sample/page.tsx
@@ -1,11 +1,8 @@
-"use client"
 
 import { prisma } from "@repo/db"
 
 export default async function Page() {
-	const users = await prisma.user.findMany()
-	console.log("USERS:", users)
 	return (
-		<div>{users.length > 0 ? users[0].email : "No USERS YET"}</div>
+		<div>sample</div>
 	)
 }
