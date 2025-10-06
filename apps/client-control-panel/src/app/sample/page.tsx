a6d9a2eab363de3ac19ad1277968a41c4b653a27
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-10-06 17:09:11 +0530

    removed sample page

--- apps/client-control-panel/src/app/sample/page.tsx
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@repo/db"
-
-export default async function Page() {
-	const users = await prisma.user.findMany()
-	console.log("USERS:kkk", users)
-	return (
-		<div>{users.length > 0 ? users[0].email : "No USERS YET"}</div>
-	)
-}
