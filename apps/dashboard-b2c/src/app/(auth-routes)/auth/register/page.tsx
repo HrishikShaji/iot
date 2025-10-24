c217e726c96c8500246785ab4e3f22db00446595
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-10-24 20:16:28 +0530

    added register in b2c

--- apps/dashboard-b2c/src/app/(auth-routes)/auth/register/page.tsx
+++ apps/dashboard-b2c/src/app/(auth-routes)/auth/register/page.tsx
@@ -1,19 +1,19 @@
-// import RegisterForm from "@/components/RegisterForm"
-// import { Suspense } from "react"
+import RegisterForm from "@/components/RegisterForm"
+import { Suspense } from "react"
 
 export default function Page() {
 	return (
 		<div>
-			{/* <Suspense fallback={ */}
-			{/* 	<div className="min-h-screen flex items-center justify-center bg-gray-50"> */}
-			{/* 		<div className="text-center"> */}
-			{/* 			<div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto"></div> */}
-			{/* 			<p className="mt-4 text-gray-600">Loading...</p> */}
-			{/* 		</div> */}
-			{/* 	</div> */}
-			{/* }> */}
-			{/* 	<RegisterForm /> */}
-			{/* </Suspense> */}
+			<Suspense fallback={
+				<div className="min-h-screen flex items-center justify-center bg-gray-50">
+					<div className="text-center">
+						<div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto"></div>
+						<p className="mt-4 text-gray-600">Loading...</p>
+					</div>
+				</div>
+			}>
+				<RegisterForm />
+			</Suspense>
 
 		</div>
 	)
